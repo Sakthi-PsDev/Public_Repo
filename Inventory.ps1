1b2ccfe8ede2141faccf54f62cbc00f04cfae156
Sakthi-Owner <sakthioffcl@outlook.com>
2024-09-03 16:59:07 +0530

    Added lines for Name,Domain and PhysicalMemory

--- Inventory.ps1
+++ Inventory.ps1
@@ -1,3 +1,14 @@
 #Inventory Script for windows machines
 #Using WMI cmdlets, advanced CIM cmdlets also available
 
+#Getting Computername and Domain info
+$Info1 = Get-CimInstance -ClassName CIM_ComputerSystem
+
+$MachineName = $Info1.Name
+
+$DomainName = $Info1.Domain
+
+$PhysicalMemory = [Math]::Round(($Info1.TotalPhysicalMemory / 1Gb),2),' Gb' 
+-join ""
+
+#Getting Drive Details
